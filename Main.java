10571c0b53e7b1eee3d4bfcf5d4ecd70ae0a6f7c
jjvyas <jishant.2407007101@vcet.edu.in>
2025-07-15 09:29:24 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Using default constructor
-        Calculator calc1 = new Calculator();
+        Calculator calc1 = new Calculator();            // Calls default constructor
+        Calculator calc2 = new Calculator(15, 25);      // Calls parameterized constructor
 
-        // Using parameterized constructor
-        Calculator calc2 = new Calculator(15, 25);
-
-        // Demonstrating method overloading
-        calc1.add();                    // No parameters
-        calc1.add(20, 30);              // Two integers
-        calc1.add(12.5f, 15.5f, 10.0f); // Three floats
-
+        calc1.add();                                    // Adds fixed numbers 5 and 10
+        calc1.add(20, 30);                             // Adds two integers
+        calc1.add(12.5f, 15.5f, 10.0f);                // Adds three floats
     }
 }
